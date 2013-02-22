4865464e2384d927e6266dbdbda9ebdb7e74a44c
Geoff Davis <gadavis@ucsd.edu>
2013-02-21 16:56:48 -0800

    Use setenforce Permissive to disable selinux
    Should work until system is rebooted.

--- manifests/disable/redhat.pp
+++ manifests/disable/redhat.pp
@@ -13,4 +13,11 @@ class selinux::disable::redhat {
         command => 'sed -i "s@^\(SELINUX=\).*@\1disabled@" /etc/sysconfig/selinux',
         unless => 'grep -q "SELINUX=disabled" /etc/sysconfig/selinux',
     }
+
+    # Put selinux into permissive mode until the system can be rebooted
+    if $::selinux_enforced {
+        exec{disable_selinux_runtime:
+            command  => '/usr/sbin/setenforce Permissive',
+        }
+    }
 }
