22ab76898e36cb0a8c5cd350862a271994b14e08
decibelHertz <1975Hz@gmail.com>
2016-02-29 07:59:28 -0800

    hotfix for old syntax

--- manifests/disable/redhat.pp
+++ manifests/disable/redhat.pp
@@ -1,23 +1,18 @@
 class selinux::disable::redhat {
-    service{restorecond:
-        ensure => stopped,
-        enable => false,
-    }
+  service { 'restorecond': ensure => 'stopped', enable => false }
+  service { 'mcstrans'   : ensure => 'stopped', enable => false }
 
-    service{mcstrans:
-        ensure => stopped,
-        enable => false,
-    }
+  exec { 'disable_selinux_sysconfig':
+    command => 'sed -i "s@^\(SELINUX=\).*@\1disabled@" /etc/sysconfig/selinux',
+    unless  => 'grep -q "SELINUX=disabled" /etc/sysconfig/selinux',
+    path    => ['/bin','/usr/bin','/sbin','/usr/sbin'],
+  }
 
-    exec{disable_selinux_sysconfig:
-        command => 'sed -i "s@^\(SELINUX=\).*@\1disabled@" /etc/sysconfig/selinux',
-        unless => 'grep -q "SELINUX=disabled" /etc/sysconfig/selinux',
+  # Put selinux into permissive mode until the system can be rebooted
+  if str2bool($::selinux_enforced) {
+    exec { 'disable_selinux_runtime' :
+      command => 'setenforce Permissive',
+      path    => ['/bin','/usr/bin','/sbin','/usr/sbin'],
     }
-
-    # Put selinux into permissive mode until the system can be rebooted
-    if str2bool($::selinux_enforced) {
-        exec{disable_selinux_runtime:
-            command  => '/usr/sbin/setenforce Permissive',
-        }
-    }
+  }
 }
